0934ba7ed45c780e80a49d0150c95772c3c9e4e4
geceatay <ilkin_mvh@mail.ru>
2026-02-01 22:09:34 +0400

    Update chechout.js

--- functions/chechout.js
+++ functions/chechout.js
@@ -1,1 +1,16 @@
 
+export const onRequest = async ({ request, env }) => {
+  const url = new URL(request.url);
+  const pathParts = url.pathname.split("/"); // /api/rooms/:id/checkout
+  const roomId = pathParts[pathParts.length - 2];
+
+  // Oda durumunu temizlik gerekiyor olarak güncelle
+  await env.otelvt
+    .prepare("UPDATE rooms SET status='cleaning' WHERE id=?")
+    .bind(roomId)
+    .run();
+
+  return new Response(JSON.stringify({ success: true }), {
+    headers: { "Content-Type": "application/json" },
+  });
+};
